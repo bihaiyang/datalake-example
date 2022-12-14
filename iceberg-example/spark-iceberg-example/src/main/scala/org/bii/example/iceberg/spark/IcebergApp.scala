c39414200d118716771971f524b9bc9238e2c9f4
bihaiyang <2217704900@qq.com>
2022-12-14 23:59:35 +0800

    add flink-example

---
+++ iceberg-example/spark-iceberg-example/src/main/scala/org/bii/example/iceberg/spark/IcebergApp.scala
@@ -0,0 +1,35 @@
+package org.bii.example.iceberg.spark
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @author bihaiyang
+ * @since 2022/11/28
+ * @desc
+ */
+object IcebergApp {
+
+  lazy val log: Logger = Logger.getLogger(this.getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder().master("local").appName("SparkOperateIceberg")
+      //指定hive catalog, catalog名称为hive_prod
+      .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkCatalog")
+      .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
+      .config("spark.sql.catalog.spark_catalog.type", "hive")
+      .config("spark.sql.catalog.local.type", "hadoop")
+      .config("spark.sql.catalog.local.warehouse", "")
+      //.config("iceberg.engine.hive.enabled", "true")
+
+      //指定hadoop catalog，catalog名称为hadoop_prod
+      .config("spark.sql.catalog.hadoop_prod", "org.apache.iceberg.spark.SparkCatalog")
+      .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
+      .config("spark.sql.catalog.hadoop_prod.type", "hadoop")
+      .config("spark.sql.catalog.hadoop_prod.warehouse", "hdfs://mycluster/sparkoperateiceberg")
+      .getOrCreate()
+
+
+
+  }
+}
